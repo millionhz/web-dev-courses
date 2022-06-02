d668946ecf124261f35193dbdc01f312b8b2fa9b
millionhz <m.waqas.hamza@gmail.com>
2022-06-02 15:25:22 +0500

    reaname to public; serve signup

--- 9-signup-form/server.js
+++ 9-signup-form/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const axios = require('axios');
 
 const app = express();
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+const signupPage = path.join(__dirname, 'public', 'signup.html');
+
 app.get('/', (req, res) => {
-  res.sendStatus(200);
+  res.sendFile(signupPage);
 });
 
 app.listen(3000);
