7fca61225741c1c531fc01ae9b9c92b585b7de04
millionhz <m.waqas.hamza@gmail.com>
2022-06-02 07:08:35 +0500

    use Promise to get weather data

--- 8-weather-api/server.js
+++ 8-weather-api/server.js
@@ -3,6 +3,7 @@ const https = require('https');
 
 const app = express();
 const APPID = '622895359404ccf3308b0f539b5ed718';
+const TIMEOUT = 10000;
 
 const geocodingApi = (city) =>
   `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APPID}`;
@@ -10,18 +11,34 @@ const geocodingApi = (city) =>
 const weatherDataApi = (lat, lon) =>
   `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APPID}&units=metric`;
 
+function getWeatherData(lat, lon) {
+  return new Promise(
+    (resolve) => {
+      https.get(weatherDataApi(lat, lon), (response) => {
+        response.on('data', (data) => {
+          resolve(JSON.parse(data));
+        });
+      });
+    },
+    (reject) => {
+      setTimeout(reject, TIMEOUT);
+    }
+  );
+}
+
 app.get('/', (req, res) => {
   https.get(geocodingApi('lahore'), (geocodingRes) => {
     geocodingRes.on('data', (data) => {
       const jsonData = JSON.parse(data)[0];
       const { lat, lon } = jsonData;
 
-      https.get(weatherDataApi(lat, lon), (weatherDataRes) => {
-        weatherDataRes.on('data', (data) => {
-          const jsonData = JSON.parse(data);
-          res.send(jsonData.main.temp.toString());
+      getWeatherData(lat, lon)
+        .then((val) => {
+          res.send(val);
+        })
+        .catch(() => {
+          res.sendStatus(500);
         });
-      });
     });
   });
 });
