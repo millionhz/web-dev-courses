450985803d78f7edba57c1740147db43face118d
millionhz <m.waqas.hamza@gmail.com>
2022-06-03 07:36:58 +0500

    implement mailchmip

--- 9-signup-form/server.js
+++ 9-signup-form/server.js
@@ -8,8 +8,49 @@ app.use(express.urlencoded({ extended: true }));
 
 const signupPage = path.join(__dirname, 'public', 'signup.html');
 
+const mailChimp = {
+  apiKey: process.env.MAILCHIMP_KEY,
+  memberUrl: 'https://us10.api.mailchimp.com/3.0/lists/ff678a82cd/members',
+
+  payload: (firstName, lastName, emailAddr) => ({
+    email_address: emailAddr,
+    status: 'subscribed',
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName,
+    },
+  }),
+};
+
+async function submitUser(firstName, lastName, emailAddr) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${mailChimp.apiKey}`,
+    },
+  };
+
+  return axios.post(
+    mailChimp.memberUrl,
+    mailChimp.payload(firstName, lastName, emailAddr),
+    options
+  );
+}
+
 app.get('/', (req, res) => {
   res.sendFile(signupPage);
+});
+
+app.post('/', (req, res) => {
+  const { firstName, lastName, email } = req.body;
+
+  submitUser(firstName, lastName, email).then(
+    (val) => {
+      res.redirect('/');
+    },
+    (reason) => {
+      res.status(500).send('submit request failed');
+    }
+  );
 });
 
 app.listen(3000);
