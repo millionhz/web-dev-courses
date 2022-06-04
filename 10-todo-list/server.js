34913bbd2d7b1ad5135f208fb8a3bbfa46760f07
millionhz <m.waqas.hamza@gmail.com>
2022-06-04 14:20:17 +0500

    implement todo functionality

--- 10-todo-list/server.js
+++ 10-todo-list/server.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const todoItems = [];
+
+function getDate() {
+  const date = new Date();
+  return date.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
+
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.render('todo-list', {
+    date: getDate(),
+    todoItems,
+  });
+});
+
+app.post('/', (req, res) => {
+  const { newTodoItem } = req.body;
+  if (newTodoItem) {
+    todoItems.push(req.body.newTodoItem);
+  }
+
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT);
