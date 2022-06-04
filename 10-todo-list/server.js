e82bad56dcb12b6236c59ccb4b9c5f49b2f21491
millionhz <m.waqas.hamza@gmail.com>
2022-06-04 16:25:40 +0500

    add style to todo list

--- 10-todo-list/server.js
+++ 10-todo-list/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
