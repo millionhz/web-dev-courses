ef8ccc718109af58479717d28b890fb5e12c1513
millionhz <m.waqas.hamza@gmail.com>
2022-06-05 07:16:03 +0500

    imoplement routes; implement error page

--- 11-blog-website/app.js
+++ 11-blog-website/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
 
+const errorPage = require('./middlewares/error');
+
 const indexRouter = require('./routes/index');
+const aboutRouter = require('./routes/about');
+const contactRouter = require('./routes/contact');
 
 const app = express();
 
@@ -12,6 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+
+app.use(errorPage);
 
 app.listen(process.env.PORT);
 
