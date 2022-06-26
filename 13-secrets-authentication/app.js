b9abc87711129963b52ed2e662906c2eefb67c6d
millionhz <m.waqas.hamza@gmail.com>
2022-06-27 02:11:27 +0500

    setup website

---
+++ 13-secrets-authentication/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.listen(3000, () => {
+  console.log(`listening on 3000`);
+});
