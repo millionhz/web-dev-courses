0128ba0b73e6e02c6173b987474d786489d13d56
millionhz <m.waqas.hamza@gmail.com>
2022-07-03 22:10:31 +0500

    add authentication to registeration

--- 13-secrets-authentication/routes/register.js
+++ 13-secrets-authentication/routes/register.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('../utils/auth');
 const User = require('../models/user');
 
 const router = new express.Router();
@@ -7,15 +8,22 @@ router.get('/', (req, res) => {
   res.render('register');
 });
 
-router.post('/', (req, res, next) => {
-  User.addUser(req.body.email, req.body.password)
-    .then(() => {
-      res.render('secrets');
-    })
-    .catch((err) => {
-      res.send('registration failed');
-      next(err);
-    });
-});
+router.post(
+  '/',
+  (req, res, next) => {
+    User.addUser(req.body.email, req.body.password)
+      .then(() => {
+        next();
+      })
+      .catch((err) => {
+        res.send('registration failed');
+        next(err);
+      });
+  },
+  passport.authenticate('local', {
+    successReturnToOrRedirect: '/secrets',
+    failureRedirect: '/login',
+  })
+);
 
 module.exports = router;
