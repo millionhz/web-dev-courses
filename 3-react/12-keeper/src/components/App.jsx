1d6a3cfe36e6ce09a864d83bac1bd73e2f7bf5fb
millionhz <m.waqas.hamza@gmail.com>
2022-07-24 12:50:45 +0500

    add components

--- 3-react/12-keeper/src/components/App.jsx
+++ 3-react/12-keeper/src/components/App.jsx
@@ -4,13 +4,15 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import notes from '../utility/notes';
+import CreateArea from './CreateArea';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((note) => (
-        <Note key={note.id} title={note.title} content={note.content} />
+        <Note key={note.key} title={note.title} content={note.content} />
       ))}
       <Footer />
     </div>
