b1a9c1dbea9cd9b9c0afe477c83603655cba0d4b
millionhz <m.waqas.hamza@gmail.com>
2022-07-24 13:02:09 +0500

    implement delete

--- 3-react/12-keeper/src/components/App.jsx
+++ 3-react/12-keeper/src/components/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../utility/notes';
+import defaultNotes from '../utility/notes';
 import CreateArea from './CreateArea';
 
 export default function App() {
+  const [notes, setNotes] = useState(defaultNotes);
+
+  function deleteNote(key) {
+    setNotes((prevNotes) => prevNotes.filter((val) => val.key !== key));
+  }
+
   return (
     <div>
       <Header />
       <CreateArea />
       {notes.map((note) => (
-        <Note key={note.key} title={note.title} content={note.content} />
+        <Note
+          key={note.key}
+          idx={note.key}
+          title={note.title}
+          content={note.content}
+          onButtonClick={deleteNote}
+        />
       ))}
       <Footer />
     </div>
--- 3-react/12-keeper/src/components/Note.jsx
+++ 3-react/12-keeper/src/components/Note.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 export default function Note(props) {
   return (
-    <div className="note">
+    <div data-key={props.idx} className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DELETE</button>
+      <button
+        onClick={(e) => {
+          props.onButtonClick(parseInt(e.target.parentNode.dataset.key));
+        }}
+      >
+        DELETE
+      </button>
     </div>
   );
 }
