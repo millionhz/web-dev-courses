c64a305a45579d68a7c4cec1b26b3d5082ba8533
millionhz <m.waqas.hamza@gmail.com>
2022-08-15 04:03:52 +0500

    remove Forkify

--- javascript-zero-to-expert/Forkify/src/js/controller.js
+++
@@ -1,9 +0,0 @@
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise((_, reject) => {
-    setTimeout(() => {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
