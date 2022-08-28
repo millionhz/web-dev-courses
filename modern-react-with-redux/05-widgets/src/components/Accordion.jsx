b3e5eef6685c0d4962036aeec4d94fdbb8919474
Muhammad Hamza <m.waqas.hamza@gmail.com>
2022-08-28 14:18:21 +0500

    implement Accordion

--- modern-react-with-redux/05-widgets/src/components/Accordion.jsx
+++ modern-react-with-redux/05-widgets/src/components/Accordion.jsx
@@ -1,9 +1,39 @@
 import React, { useState } from 'react';
 
-function Accordion() {
-  const [activeIndex, setActiveIndex] = useState(0);
+function Accordion({ list }) {
+  const [activeIndex, setActiveIndex] = useState(-1);
 
-  return <div>Accordion</div>;
+  function onTitleClick(idx) {
+    setActiveIndex((prevActiveIndex) => {
+      if (prevActiveIndex === idx) {
+        return -1;
+      }
+      return idx;
+    });
+  }
+
+  function isActive(idx) {
+    return idx === activeIndex ? 'active' : '';
+  }
+
+  return (
+    <div className="ui styled accordion">
+      {list.map((item, idx) => (
+        <React.Fragment key={idx}>
+          <div
+            className={`${isActive(idx)} title`}
+            onClick={() => onTitleClick(idx)}
+          >
+            <i className="dropdown icon"></i>
+            {item.title}
+          </div>
+          <div className={`${isActive(idx)} content`}>
+            <p>{item.content}</p>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
 
 export default Accordion;
--- modern-react-with-redux/05-widgets/src/components/App.jsx
+++ modern-react-with-redux/05-widgets/src/components/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const devData = [
+  {
+    title: 'What is a dog?',
+    content:
+      ' A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+  },
+  {
+    title: 'What kinds of dogs are there?',
+    content:
+      'There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.',
+  },
+];
 
 function App() {
-  return <h1>Hello</h1>;
+  return <Accordion list={devData} />;
 }
 
 export default App;
