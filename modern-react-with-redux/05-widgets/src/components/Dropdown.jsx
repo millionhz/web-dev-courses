335dad9ab77e78987bb30d63443f73d48d21a4c5
Muhammad Hamza <m.waqas.hamza@gmail.com>
2022-08-29 09:24:11 +0500

    implement drop down close

--- modern-react-with-redux/05-widgets/src/components/Dropdown.jsx
+++ modern-react-with-redux/05-widgets/src/components/Dropdown.jsx
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 function Dropdown({ title, options, selected, onSelectedChange }) {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current.contains(e.target)) {
+        return;
+      }
+      setOpen(false);
+    }
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
 
   function renderSelections() {
     return options.map(({ label, value }, idx) => {
@@ -24,12 +40,15 @@ function Dropdown({ title, options, selected, onSelectedChange }) {
   }
 
   return (
-    <div className="ui form">
+    <div
+      ref={ref}
+      className="ui form"
+      onClick={() => setOpen((prevOpen) => !prevOpen)}
+    >
       <div className="field">
         <label className="label">{title}</label>
         <div
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-          onClick={() => setOpen((prevIsOpen) => !prevIsOpen)}
         >
           <i className="dropdown icon"></i>
           <div className="text">{options[selected].label}</div>
